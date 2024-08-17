87e3e8bdc9bc542583157fcfe7ea6c710a54510c
Yuki (aka Rubén Gómez) <rugoli@gmail.com>
2024-08-17 20:36:43 +0200

    Finished the tutorial
    Time to try something by me?

--- main.gd
+++ main.gd
@@ -17,6 +17,8 @@ func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -25,6 +27,7 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 
 func _on_mob_timer_timeout() -> void:
@@ -55,6 +58,7 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout() -> void:
